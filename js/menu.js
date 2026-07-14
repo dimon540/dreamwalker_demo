cda358396482faaa7984ac47973bbc5bbd019010
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-14 14:02:56 +0300

    Add menu structure to the app

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,32 @@
 console.log("Меню Dreamwalker завантажено.");
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<div class="menu">
+
+    <h1 class="logo">Dreamwalker</h1>
+
+    <div class="menu-buttons">
+
+        <button>Нова гра</button>
+
+        <button>Продовжити</button>
+
+        <button>Розділи</button>
+
+        <button>Збереження</button>
+
+        <button>Налаштування</button>
+
+        <button>Досягнення</button>
+
+        <button>Галерея</button>
+
+        <button>Про авторів</button>
+
+        <button>Вийти</button>
+
+    </div>
+
+</div>
+`;

4e0f2c2f67d9d7eb06eca9f41bf327e9833c8178
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 17:25:31 +0300

    Refactor scene1 structure and update steps

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -1,25 +1,52 @@
 const scenes = {
 
+    scene1: {
 
-scene1:{
+        steps: [
 
-
-background:
-"assets/backgrounds/scene1.jpg",
-
-
-
-text:
-
-"Перед очима відкривається величезний футуристичний мегаполіс. Високі хмарочоси губляться серед світла і хмар.",
+            {
+                background: "assets/backgrounds/scene1.jpg",
+                text: "Перед очима відкривається величезний футуристичний мегаполіс."
+            },
 
 
-next:null
+            {
+                background: "assets/backgrounds/scene1_2.jpg",
+                text: "Високі хмарочоси здіймаються далеко вгору, гублячись серед хмар і світла."
+            },
 
 
+            {
+                background: "assets/backgrounds/scene1_3.jpg",
+                text: "На будівлях сяють величезні голографічні екрани. Десь далеко лунає музика."
+            },
 
-}
+
+            {
+                background: "assets/backgrounds/scene1_4.jpg",
+                text: "Вулиці наповнені людьми. Вони поспішають у своїх справах, нікого не помічаючи."
+            },
 
 
+            {
+                background: "assets/backgrounds/scene1_5.jpg",
+                text: "Серед цього величезного міста виникає дивне відчуття... ніби це місце тобі незнайоме."
+            },
+
+
+            {
+                background: "assets/backgrounds/scene1_6.jpg",
+                text: "Навколо стільки людей, але здається, що ніхто тебе не бачить."
+            },
+
+
+            {
+                background: "assets/backgrounds/scene1_7.jpg",
+                text: "Чому здається, що ти тут чужа?"
+            }
+
+        ]
+
+    }
 
 };

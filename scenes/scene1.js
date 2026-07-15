b5fb18411a549dbf30cb88fc425e990b5d7c36ff
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 18:04:40 +0300

    Revise scene text and background images

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -9,49 +9,28 @@ steps: [
 background:"assets/backgrounds/scene1.jpg",
 
 text:
-"Світло міста повільно розчинялося у нічному небі.\n\nСаме сьогодні починається твоя нова історія..."
+"Екран повільно з'являється з темряви.\n\nПеред очима відкривається величезний мегаполіс майбутнього."
 },
-
 
 
 {
 background:"assets/backgrounds/scene1.jpg",
 
 text:
-"Екран повільно з'являється з темряви.\n\nПеред очима відкривається величезний мегаполіс майбутнього."
+"Високі будівлі сягають далеко вгору.\n\nМіж ними безшумно пролітають повітряні машини.\n\nСотні людей рухаються вулицями, кожен зайнятий своїми справами."
 },
-
 
 
 {
 background:"assets/backgrounds/scene1_2.jpg",
 
 text:
-"Високі хмарочоси здіймаються далеко вгору, гублячись серед хмар і світла.\n\nЗдається, цьому місту немає кінця."
+"Навколо тисячі людей.\n\nХтось поспішає.\n\nХтось розмовляє.\n\nХтось просто йде, не помічаючи нічого навколо."
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_3.jpg",
-
-text:
-"На будівлях сяють величезні голографічні екрани.\n\nДесь грає музика.\n\nМісто живе власним життям."
-},
-
-
-
-{
-background:"assets/backgrounds/scene1_4.jpg",
-
-text:
-"Повітряні машини безшумно пролітають між будівлями.\n\nВулицями рухаються сотні людей."
-},
-
-
-
-{
-background:"assets/backgrounds/scene1_4.jpg",
+background:"assets/backgrounds/scene1_2.jpg",
 
 name:"Єва",
 
@@ -60,9 +39,8 @@ text:
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_4.jpg",
+background:"assets/backgrounds/scene1_2.jpg",
 
 name:"Єва",
 
@@ -71,67 +49,42 @@ text:
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_5.jpg",
+background:"assets/backgrounds/scene1_3.jpg",
 
 text:
-"Люди проходять поруч.\n\nХтось розмовляє.\n\nХтось сміється.\n\nХтось дивиться у телефон."
+"Ти піднімаєш погляд.\n\nВеличезні хмарочоси губляться серед світла і хмар."
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_5.jpg",
-
-text:
-"Але ніхто не звертає уваги.\n\nНаче тебе тут немає."
-},
-
-
-
-{
-background:"assets/backgrounds/scene1_5.jpg",
+background:"assets/backgrounds/scene1_3.jpg",
 
 name:"Єва",
 
 text:
-"Дивно..."
+"Таке відчуття, ніби це місце мені незнайоме..."
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_5.jpg",
-
-name:"Єва",
+background:"assets/backgrounds/scene1_4.jpg",
 
 text:
-"Таке відчуття, ніби мене ніхто не помічає."
+"Твій погляд зупиняється на величезному голографічному екрані."
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_6.jpg",
+background:"assets/backgrounds/scene1_4.jpg",
 
 text:
-"Твій погляд піднімається до величезного голографічного екрану."
+"На ньому виступає відома співачка перед багатотисячною аудиторією.\n\nЇї музика лунає на все місто."
 },
 
 
-
 {
-background:"assets/backgrounds/scene1_6.jpg",
-
-text:
-"На ньому виступає відома співачка перед багатотисячною аудиторією.\n\nЇї голос чує все місто."
-},
-
-
-
-{
-background:"assets/backgrounds/scene1_6.jpg",
+background:"assets/backgrounds/scene1_4.jpg",
 
 name:"Єва",
 
@@ -140,6 +93,39 @@ text:
 },
 
 
+{
+background:"assets/backgrounds/scene1_4.jpg",
+
+name:"Єва",
+
+text:
+"Напевно, це приємно...\n\nКоли тебе знають."
+},
+
+
+{
+background:"assets/backgrounds/scene1_5.jpg",
+
+text:
+"Через кілька секунд ти відводиш погляд від екрана.\n\nМісто продовжує жити своїм життям."
+},
+
+
+{
+background:"assets/backgrounds/scene1_5.jpg",
+
+text:
+"Люди сміються.\n\nРозмовляють.\n\nКудись поспішають."
+},
+
+
+{
+background:"assets/backgrounds/scene1_6.jpg",
+
+text:
+"Попереду з'являється величезний небоскреб.\n\nЙого вершина ховається десь серед світла."
+},
+
 
 {
 background:"assets/backgrounds/scene1_6.jpg",
@@ -147,27 +133,26 @@ background:"assets/backgrounds/scene1_6.jpg",
 name:"Єва",
 
 text:
-"Напевно, це приємно, коли тебе знають."
+"Він здається безкінечним..."
 },
 
+
+{
+background:"assets/backgrounds/scene1_6.jpg",
+
+name:"Єва",
+
+text:
+"Цікаво...\n\nЧи є межа тому, наскільки високо можна піднятися?"
+},
 
 
 {
 background:"assets/backgrounds/scene1_7.jpg",
 
 text:
-"Через кілька секунд ти відводиш погляд від екрана.\n\nМісто продовжує жити."
+"Ти знову дивишся на місто.\n\nКожен кудись іде.\n\nНаче всі знають, чого хочуть від життя."
 },
-
-
-
-{
-background:"assets/backgrounds/scene1_7.jpg",
-
-text:
-"Кожен кудись іде.\n\nКожен має свою мету.\n\nНаче всі знають, чого хочуть від життя."
-},
-
 
 
 {

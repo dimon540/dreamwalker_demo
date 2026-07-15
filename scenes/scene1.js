c7fc78797e2fb518b459998c2ac7031c856a1a98
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 16:27:26 +0300

    Enhance scene1 with narrative and dialogue
    Added narrative steps and dialogue to scene1.

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -1,13 +1,68 @@
 const scenes = {
 
-    scene1: {
 
-        background: "./assets/backgrounds/scene1.jpg",
+scene1:{
 
-        text: "Світло міста розчинялося у нічному небі. Саме сьогодні починається твоя нова історія...",
 
-        next: null
+background:"./assets/backgrounds/scene1.jpg",
 
-    }
+
+steps:[
+
+
+{
+
+type:"text",
+
+text:"Перед очима відкривається величезне місто."
+
+},
+
+
+{
+
+type:"text",
+
+text:"Високі хмарочоси здіймаються далеко вгору, ховаючись серед світла і хмар."
+
+},
+
+
+{
+
+type:"dialogue",
+
+speaker:"player",
+
+text:"Де я?.."
+
+},
+
+
+{
+
+type:"dialogue",
+
+speaker:"player",
+
+text:"Я не пам'ятаю, як сюди потрапила."
+
+},
+
+
+{
+
+type:"thought",
+
+text:"Дивно... Таке відчуття, ніби мене ніхто не помічає."
+
+}
+
+
+]
+
+
+}
+
 
 };

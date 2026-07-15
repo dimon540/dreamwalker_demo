3497c95e70b8a08db1b4aac23113cbd3458f2489
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 17:36:35 +0300

    Refactor scene1 structure and update content

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -1,52 +1,91 @@
 const scenes = {
 
-    scene1: {
 
-        steps: [
-
-            {
-                background: "assets/backgrounds/scene1.jpg",
-                text: "Перед очима відкривається величезний футуристичний мегаполіс."
-            },
+scene1:{
 
 
-            {
-                background: "assets/backgrounds/scene1_2.jpg",
-                text: "Високі хмарочоси здіймаються далеко вгору, гублячись серед хмар і світла."
-            },
+steps:[
 
 
-            {
-                background: "assets/backgrounds/scene1_3.jpg",
-                text: "На будівлях сяють величезні голографічні екрани. Десь далеко лунає музика."
-            },
+{
+
+background:
+"assets/backgrounds/scene1.jpg",
 
 
-            {
-                background: "assets/backgrounds/scene1_4.jpg",
-                text: "Вулиці наповнені людьми. Вони поспішають у своїх справах, нікого не помічаючи."
-            },
+text:
+"Світло міста розчинялося у нічному небі. Саме сьогодні починається твоя нова історія..."
 
 
-            {
-                background: "assets/backgrounds/scene1_5.jpg",
-                text: "Серед цього величезного міста виникає дивне відчуття... ніби це місце тобі незнайоме."
-            },
+},
 
 
-            {
-                background: "assets/backgrounds/scene1_6.jpg",
-                text: "Навколо стільки людей, але здається, що ніхто тебе не бачить."
-            },
+
+{
+
+background:
+"assets/backgrounds/scene1_2.jpg",
 
 
-            {
-                background: "assets/backgrounds/scene1_7.jpg",
-                text: "Чому здається, що ти тут чужа?"
-            }
+text:
+"Перед очима відкривається величезний мегаполіс. Високі хмарочоси здіймаються далеко вгору..."
 
-        ]
 
-    }
+},
+
+
+
+{
+
+background:
+"assets/backgrounds/scene1_3.jpg",
+
+
+text:
+"На будівлях сяють величезні голографічні екрани."
+
+
+},
+
+
+
+{
+
+background:
+"assets/backgrounds/scene1_4.jpg",
+
+
+name:"Єва",
+
+
+text:
+"Де я?.."
+
+
+},
+
+
+
+{
+
+background:
+"assets/backgrounds/scene1_5.jpg",
+
+
+name:"Єва",
+
+
+text:
+"Я не пам'ятаю, як сюди потрапила..."
+
+
+}
+
+
+]
+
+
+}
+
 
 };

4b6feb7393a551bae88b323e1e348b2fde21b382
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 15:45:48 +0300

    Fix background path and update next scene reference

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -1,7 +1,13 @@
 const scenes = {
+
     scene1: {
-        background: "assets/backgrounds/scene1.jpg",
+
+        background: "./assets/backgrounds/scene1.jpg",
+
         text: "Світло міста розчинялося у нічному небі. Саме сьогодні починається твоя нова історія...",
-        next: "scene2"
+
+        next: null
+
     }
+
 };

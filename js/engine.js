9520b3259c1753399cddcae2f0a07f014db2bdc8
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 14:54:34 +0300

    Add functions to load scene and start game

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,13 @@
+function loadScene(sceneName) {
+    const scene = scenes[sceneName];
 
+    document.getElementById("background").style.backgroundImage =
+        `url(${scene.background})`;
+
+    document.getElementById("dialogue").innerText =
+        scene.text;
+}
+
+function startGame() {
+    loadScene("scene1");
+}

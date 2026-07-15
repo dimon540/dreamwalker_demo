124ab5a1c468298ff609a3731496e8e9647e8259
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 14:53:41 +0300

    Add scene1 with background and text details

---
+++ scenes/scene1.js
@@ -0,0 +1,7 @@
+const scenes = {
+    scene1: {
+        background: "assets/backgrounds/scene1.jpg",
+        text: "Світло міста розчинялося у нічному небі. Саме сьогодні починається твоя нова історія...",
+        next: "scene2"
+    }
+};

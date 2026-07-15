8c15118c154cb32b5df14393ffafbc16e0c38da6
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-15 15:39:54 +0300

    Implement scene management functions in game engine

--- js/game.js
+++ js/game.js
@@ -1,1 +1,75 @@
+// =========================
+// GAME ENGINE
+// =========================
+
 console.log("Двигун гри запущено.");
+
+
+// запуск сцени
+
+function startScene(sceneName) {
+
+
+    const scene = scenes[sceneName];
+
+
+    if (!scene) {
+
+        console.error("Сцена не знайдена:", sceneName);
+
+        return;
+
+    }
+
+
+
+    app.innerHTML = `
+
+    <div class="screen">
+
+
+        <div id="background"></div>
+
+
+        <div id="dialogue">
+
+            ${scene.text}
+
+        </div>
+
+
+
+        <button class="game-button" onclick="nextScene('${scene.next}')">
+
+            Далі
+
+        </button>
+
+
+    </div>
+
+
+    `;
+
+
+
+    document.getElementById("background").style.backgroundImage =
+    `url('${scene.background}')`;
+
+}
+
+
+
+// наступна сцена
+
+function nextScene(sceneName) {
+
+
+    if(sceneName){
+
+        startScene(sceneName);
+
+    }
+
+
+}
